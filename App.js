d85c596ee0a21130f284e9e974b1709c0a4f2bc7
phamtuquocminh <minhptq@gmail.com>
2022-12-14 22:58:33 +0700

    vscode autosave

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open nua ha</Text>
+      <Text>Open nua ha tiep tuc</Text>
       <StatusBar style="auto" />
     </View>
   );
