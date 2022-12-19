fc660975df00478195f3d6be24007c90bf6d0faa
phamtuquocminh <minhptq@gmail.com>
2022-12-19 22:06:17 +0700

    vscode autosave

--- screens/Login.js
+++ screens/Login.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Login() {
   return (
     <View style={styles.container}>
-            <Text></Text>
+          <Text></Text>
     </View>
   );
   }
